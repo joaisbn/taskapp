b093e4a954f33c53df373e439e1a9d5eb41c9411
joaisbn <joaisbn@gmail.com>
2023-10-26 14:09:01 -0300

    adicionando paginas home e task

--- app/Controllers/Tasks.php
+++ app/Controllers/Tasks.php
@@ -2,10 +2,10 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
+class Tasks extends BaseController
 {
     public function index(): string
     {
-        return view('Home/index');
+        return view('Tasks/index');
     }
 }
---
+++ app/Views/Tasks/index.php
@@ -0,0 +1,7 @@
+<?= $this->extend('layouts/default') ?>
+
+<?= $this->section('title') ?>Tasks<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+    <h1>Tasks</h1>
+<?= $this->endSection() ?>
